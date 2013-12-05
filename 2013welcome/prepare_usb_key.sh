d562628a7036786771e3e943427d127766b0c0e1
Jonas Wagner <jonas.wagner@epfl.ch>
2013-12-05 21:52:07 +0100

    Add .gitignore, and some missing non-generated files.

---
+++ 2013welcome/prepare_usb_key.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e
+
+copy_key() {
+    device="$1"
+    statefile="state_$(basename $device)"
+    if ! [ -e "$device" ] || [ -e "$statefile" ]; then
+        return
+    fi
+    touch "$statefile"
+
+    echo "copying iso to $device"
+    sleep 5
+    for i in 1 2 3 4; do
+        if mount | grep -q "$device$i"; then
+            umount "$device$i"
+        fi
+    done
+
+    cp 2013welcome.iso "$device"
+
+    echo "done copying to $device. Please remove it."
+
+    while [ -e "$device" ]; do
+        sleep 1
+    done
+
+    rm $statefile
+}
+
+main() {
+
+    while true; do
+        for device in /dev/sdf /dev/sdg /dev/sdh /dev/sdi; do
+            copy_key $device &
+        done
+
+        sleep 3
+    done
+}
+
+main
