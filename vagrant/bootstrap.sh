9f1a4b4be4662e374ddc7598dd0d106cbd97f968
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-02-11 17:52:34 +0100

    Added Vagrant support, moved build to client/ and clean up

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Setup apt-cacher-ng
+if [ `nc -z 127.0.0.1 3142` ]; then
+	echo "Host has a working apt-cacher daemon, using the host's one"
+else
+	echo "Could not find a working apt proxy, installing one locally"
+	apt-get update > /dev/null
+	apt-get install -y apt-cacher-ng
+
+	echo "CacheDir: /vagrant/.cache/apt-cacher-ng" > /etc/apt-cacher-ng/zzz_override.conf
+	systemctl restart apt-cacher-ng
+fi
+echo "Acquire::http::proxy \"http://127.0.0.1:3142\";" > /etc/apt/apt.conf.d/01proxy
+
+
+# Setup packages mirrors
+echo "deb http://httpredir.debian.org/debian jessie-updates main" >> /etc/apt/sources.list.d/jessie-updates.list
+echo "deb-src http://httpredir.debian.org/debian jessie-updates main" >> /etc/apt/sources.list.d/jessie-updates.list
+
+# update
+apt-get update
+apt-get upgrade -y
+apt-get install -y live-build
+
+# setup build directory
+mkdir -p /home/vagrant/client/{auto,config}
+chown vagrant:vagrant /home/vagrant/client
+
+mount --rbind {,/home}/vagrant/client/auto
+mount --rbind {,/home}/vagrant/client/config
+
+sh /vagrant/vagrant/install_live_build_with_uefi.sh
---
+++ vagrant/install_live_build_with_uefi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+base_url="http://http.kali.org/pool/main/l/live-build/"
+package=`curl ${base_url} 2>/dev/null | grep ".deb" | tr \" "\n" | grep ".deb" | grep -v ">" | grep -v "kali" `
+
+echo "Install live-build with UEFI support*"
+
+curl -L -o "/tmp/${package}" "${base_url}${package}" 2>/dev/null
+
+dpkg -i "/tmp/${package}"
+rm "/tmp/${package}"
+
